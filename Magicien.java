5143d8629e2c73a478ec1ed53f5e46d95444b375
VithuXD <84857358+VithuXD@users.noreply.github.com>
2021-05-29 17:33:31 -0400

    Add files via upload

---
+++ Magicien.java
@@ -0,0 +1,357 @@
+////////////////////
+//
+// V2 KUMAR GNANA5AM6
+// Le 18 Fevrier 2021
+// Projet d'animation 
+//
+//////////////////// 
+
+public class Magicien{
+    public static void main(String [] args) throws InterruptedException{
+        
+        while(true){
+         // Cette animation est a propos d'un magicien qui fait de la magique!!
+         //Cadre vide
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         //Cadre 1 : Le magicien est dans l'auditorium avec la public et salut tout le monde
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |              __             |   ");
+         System.out.println("  |            _|_|_            |   ");
+         System.out.println("  |              O -bonjour!    |   ");
+         System.out.println("  |             /|\\             |   ");
+         System.out.println("  |              | \\            |   ");
+         System.out.println("  |    ________ / \\_+___________|   ");
+         System.out.println("  |---|           Woohoo            ");
+         System.out.println("  |---|   \\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("    HII------------------------     "); 
+         System.out.println(" \\O/ \\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  -------SLT---------------OH YAA!  ");
+         System.out.println(" \\O/ \\O/ \\O/\\O/\\O/ \\O/\\O/    ");
+         
+         // Cadre vide
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         // Cadre 2: Le magicien commence la magie
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |              __             |   ");
+         System.out.println("  |            _|_|_  +         |   ");
+         System.out.println("  |On commence!- O   /          |   ");
+         System.out.println("  |             /|\\/            |   ");
+         System.out.println("  |              |              |   ");
+         System.out.println("  |    ________ / \\_____________|   ");
+         System.out.println("  |---|                ouiii        ");
+         System.out.println("  |---|   \\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("    Swag-----HELL YA------------    "); 
+         System.out.println(" \\O/ \\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  ----OOH CA COMMENCE!------------  ");
+         System.out.println(" \\O/ \\O/ \\O/ \\O/\\O/ \\O/\\O/   ");
+         
+         // Cadre Vide
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         // Cadre 3: Il fait de la magie!!
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |            __               |   ");
+         System.out.println("  |           _|_|_+            |   ");
+         System.out.println("  |Abracadabra!- O \\            |   ");
+         System.out.println("  |             /|\\/            |   ");
+         System.out.println("  |              |              |   ");
+         System.out.println("  |    ________ / \\_____________|   ");
+         System.out.println("  |---|    Abracadabra              ");
+         System.out.println("  |---|   \\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("    -----------------Cendrillon-----"); 
+         System.out.println("  \\O/\\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  tadaaa!---Dude not now----------  ");
+         System.out.println("  \\O/\\O/ \\O/ \\O/\\O/ \\O/\\O/   ");
+        
+         // Cadre vide
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         // Cadre 4:
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |              __             |   ");
+         System.out.println("  |            _|_|_            |   ");
+         System.out.println("  |              O -TADAAAAA    |   ");
+         System.out.println("  |             /|\\             |   ");
+         System.out.println("  |              | \\            |   ");
+         System.out.println("  |    ________ / \\_+___________|   ");
+         System.out.println("  |---|           ouuuu             ");
+         System.out.println("  |---|  \\O/ \\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("    smtg abt to pop out--------     "); 
+         System.out.println(" \\O/ \\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  -------hahaha------------j peur   ");
+         System.out.println(" \\O/ \\O/ \\O/ \\O/\\O/ \\O/\\O/   ");
+         
+         // Cadre vide:
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         // Cadre 5: Il fait apparaitre un lapin
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |              __             |   ");
+         System.out.println("  |            _|_|_            |   ");
+         System.out.println("  |              O - Merci!     |   ");
+         System.out.println("  |             /|\\    (\\_/)    |   ");
+         System.out.println("  |              | \\_+ ('x')    |   ");
+         System.out.println("  |    ________ / \\ (\") (\")_____|   ");
+         System.out.println("  |---|                youpii       ");
+         System.out.println("  |---|.  \\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("    c un lapin!--------Bravo----    "); 
+         System.out.println(" \\O/ \\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  -----OMGG---------U got this man  ");
+         System.out.println(" \\O/ \\O/\\O/ \\O/ \\O/ \\O/\\O/   ");
+        
+         //Cadre vide:
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         // Cadre 6: Il demande un volontaire a l'audiance 
+         System.out.println("   _____________________________   ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |              __             |   ");
+         System.out.println("  |            _|_|_            |   ");
+         System.out.println("  |              O -Volontaire? |   ");
+         System.out.println("  |             /|\\             |   ");
+         System.out.println("  |              | \\            |   ");
+         System.out.println("  |    ________ / \\ + __________|   ");
+         System.out.println("  |---|                moi!         ");
+         System.out.println("  |---|    O/ O/ O O/ O O           ");
+         System.out.println("    magic dude, ici!---je suis la!! "); 
+         System.out.println("   O/  O  \\O  O/  O/  O  O/   \\O  ");
+         System.out.println("  -----SVP-------------broo-------- ");
+         System.out.println("   O/  Ov   O/  O \\O   O/  O  \\O  ");
+        
+         // Cadre vide
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         // Cadre 7: Il a choisis quelqu'un qui a pas lever la main lol
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |              __             |   ");
+         System.out.println("  |            _|_|_            |   ");
+         System.out.println("  |              O -Toi! avec le|   ");
+         System.out.println("  |             /|\\ peace sign! |   ");
+         System.out.println("  |              | \\            |   ");
+         System.out.println("  |    ________ / \\ +___________|   ");
+         System.out.println("  |---|    chanceux!                ");
+         System.out.println("  |---|     O/  O/  O   O/  O    O  ");
+         System.out.println("    Je savais------------aww man--- "); 
+         System.out.println("   O/  O   \\O  O/  O/  O   O/ \\O  ");
+         System.out.println("  -----J meme pas lever ma main bruh");
+         System.out.println("   O/  Ov   O/  O  \\O   O/ \\O     ");
+        
+         // Cadre vide:
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         //Cadre 8: Il fait de la magie sur lui
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |        __                   |   ");
+         System.out.println("  |       _|_|Abracadabra!      |   ");
+         System.out.println("  |         O/         O -Oh god|   ");
+         System.out.println("  |        /|\\       /|\\        |   ");
+         System.out.println("  |         | \\       |         |   ");
+         System.out.println("  |    ___ / \\_+_____/ \\________|   ");
+         System.out.println("  |---|  he abt to disappear        ");
+         System.out.println("  |---|   \\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("    -------------whats gonna happen-"); 
+         System.out.println(" \\O/ \\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  ----abracadabraaaa-------------   ");
+         System.out.println(" \\O/     \\O/ \\O/\\O/ \\O/\\O/    ");
+        
+         //Cadre vide:
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         //Cadre 9: Le volontaire a peur et demande de l'aide 
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |        __                   |   ");
+         System.out.println("  |       _|_|_.                |   ");
+         System.out.println("  |         O -TADAAAA O -AHHH  |   ");
+         System.out.println("  |        /|\\       /|\\Aide    |   ");
+         System.out.println("  |.        | \\       |   moi!! |   ");
+         System.out.println("  |    ___ / \\_+_____/ \\________|   ");
+         System.out.println("  |---|      HAHA                   ");
+         System.out.println("  |---|   \\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("    -SWAG---------------Hes dead----"); 
+         System.out.println(" \\O/ \\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  -----oh no----------------------  ");
+         System.out.println("   \\O/   \\O/ \\O/\\O/ \\O/\\O/    ");
+    
+         // Cadre vide:
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         System.out.println("");
+         
+         //Cadre 10:Finalement, le magicien a fait disparru le vonlontaire
+         System.out.println("   _____________________________    ");
+         System.out.println("  |  BIENVENUE A MON SPECTACLE  |   ");
+         System.out.println("  |          DE MAGIE           |   ");
+         System.out.println("  |       __                    |   ");
+         System.out.println("  |      _|_|_                  |   ");
+         System.out.println("  |        O -hehe merci^^      |   ");
+         System.out.println("  |       /|\\                   |   ");
+         System.out.println("  |        | \\        POUF!     |   ");
+         System.out.println("  |    ___/ \\_+_________________|   ");
+         System.out.println("  |---|    IL A DISPARRU!!          ");
+         System.out.println("  |---|   \\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("   Swag-----------------Fantastique!"); 
+         System.out.println(" \\O/ \\O/\\O/\\O/\\O/\\O/\\O/\\O/  ");
+         System.out.println("  ---------Welp, son aint comin back");
+         System.out.println(" \\O/      \\O/ \\O/\\O/ \\O/\\O/   ");
+         
+         Thread.sleep(100);
+        }
+    }
+}
